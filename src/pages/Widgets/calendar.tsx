35515dd73532064dca98295e0af18c4d9bee43cc
Isa8205 <156502124+Isa8205@users.noreply.github.com>
2025-03-30 20:01:16 +0300

    Added a modal to the calendar for updating the events

--- src/pages/Widgets/calendar.tsx
+++ src/pages/Widgets/calendar.tsx
@@ -1,13 +1,15 @@
+import axios from 'axios';
 import {
+  CalendarDays,
   ChevronLeftCircle,
   ChevronRightCircle,
   X,
 } from 'lucide-react';
 import React, { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 
 // Utility function to generate the days of the current month
 const generateCalendarDays = (year: number, month: number) => {
-  const firstDay = new Date(year, month, 1);
   const lastDay = new Date(year, month + 1, 0);
   const daysInMonth = lastDay.getDate();
   const days: Date[] = [];
@@ -32,12 +34,118 @@ interface Event {
 }
 
 const Calendar: React.FC = () => {
+  const [modalDisp, setModalDisp] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const EventAddModal = () => {
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+      const formData = new FormData(e.currentTarget)
+      const data = Object.fromEntries(formData.entries())
+
+      const response = await axios.post('http://localhost:3000/api/calendar/add-event', {...data, date: selectedDate})
+      console.log(response)
+    };
+
+    return (
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 text-black z-50">
+        <ToastContainer />
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white rounded-lg shadow-lg w-full max-w-md"
+        >
+          {/* Dialog Header */}
+          <div className="border-b p-4">
+            <span className="flex items-center justify-between">
+              <h2 className="text-lg font-semibold inline-flex gap-3">
+                <CalendarDays /> Add New Event for{' '}
+                {selectedDate?.toDateString()}
+              </h2>
+              <X
+                className="bg-gray-100 cursor-pointer hover:bg-red-500 hover:text-white rounded-sm"
+                onClick={() => setModalDisp(false)}
+              />
+            </span>
+            <p className="text-sm text-gray-600">
+              Enter the details for the new Event.
+            </p>
+          </div>
+
+          {/* Dialog Content */}
+          <div className="p-4">
+            <div className="flex flex-col gap-2 items-stretch">
+              {/* FarmerNumber Input */}
+              <div className="inline-flex flex-col items-start  justify-stretch">
+                <label
+                  htmlFor="event-name"
+                  className="text-right font-medium text-sm text-gray-700"
+                >
+                  Event Name
+                </label>
+                <input
+                  required
+                  type="text"
+                  id="event-name"
+                  name="title"
+                  className="col-span-3 p-2 border rounded-md focus:outline-none "
+                />
+              </div>
+
+              <div className="flex justify-between">
+                {/* StartTime Input */}
+                <div className="flex flex-col">
+                  <label
+                    htmlFor="end-time"
+                    className="text-right font-medium text-sm text-gray-700"
+                  >
+                    Start Time
+                  </label>
+                  <input className='col-span-3 p-2 border rounded-md focus:outline-none ' type="time" name='startTime' id='end-time' />
+                </div>
+
+                {/* EndTime Input */}
+                <div className="flex flex-col">
+                  <label
+                    htmlFor="end-time"
+                    className="text-right font-medium text-sm text-gray-700"
+                  >
+                    End Time
+                  </label>
+                  <input type="time" className='col-span-3 p-2 border rounded-md focus:outline-none ' name='endtime' id='end-time' />
+                </div>
+              </div>
+
+              {/* Quantity Input */}
+              <div className="flex flex-col items-start">
+                <label
+                  htmlFor="quantity"
+                  className="text-right font-medium text-sm text-gray-700"
+                >
+                  Venue
+                </label>
+                <select className='col-span-3 p-2 border rounded-md focus:outline-none' name="location" id="event-location">
+                  <option value="Dome">Dome</option>
+                  <option value="Hall">Hall</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          {/* Dialog Footer */}
+          <div className="border-t p-4 flex justify-end">
+            <button className="bg-gray-100 text-gray-600 border-2 border-accent hover:text-white py-2 px-4 rounded-md hover:bg-accent focus:outline-none">
+              Add Delivery
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  };
+
   const today = new Date();
   const [currentMonth, setCurrentMonth] = useState(today.getMonth());
   const [currentYear, setCurrentYear] = useState(today.getFullYear());
   const [events, setEvents] = useState<Event[]>([]);
   const [newEvent, setNewEvent] = useState<string>('');
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
   // Get the current month's days and the first day of the month
   const daysInMonth = generateCalendarDays(currentYear, currentMonth);
@@ -83,46 +191,50 @@ const Calendar: React.FC = () => {
 
   return (
     <div className="">
+      {modalDisp && <EventAddModal />}
       <header className="text-center mb-4">
         <div className="flex justify-between items-center">
           <div className="text-3xl font-semibold">
             {formatMonth(currentMonth)} {currentYear}
           </div>
-          <div className='inline-flex gap-4'>
+          <div className="inline-flex gap-4">
             <button
-            className='px-4 py-2 bg-gray-500 text-white rounded-md border'
+              className="px-4 py-2 bg-gray-500 text-white rounded-md border"
               onClick={() => {
                 setCurrentMonth(today.getMonth());
                 setCurrentYear(today.getFullYear());
-              }}>Today</button>
-              <span>
+              }}
+            >
+              Today
+            </button>
+            <span>
               <button
-              onClick={() => {
-                if (currentMonth === 0) {
-                  setCurrentMonth(11);
-                  setCurrentYear(currentYear - 1);
-                } else {
-                  setCurrentMonth(currentMonth - 1);
-                }
-              }}
-              className="px-4 py-2 bg-gray-500 text-white rounded-l-md border-r"
-            >
-              <ChevronLeftCircle />
-            </button>
-            <button
-              onClick={() => {
-                if (currentMonth === 11) {
-                  setCurrentMonth(0);
-                  setCurrentYear(currentYear + 1);
-                } else {
-                  setCurrentMonth(currentMonth + 1);
-                }
-              }}
-              className="px-4 py-2 bg-gray-500 text-white rounded-r-md border-l"
-            >
-              <ChevronRightCircle />
-            </button>
-              </span>
+                onClick={() => {
+                  if (currentMonth === 0) {
+                    setCurrentMonth(11);
+                    setCurrentYear(currentYear - 1);
+                  } else {
+                    setCurrentMonth(currentMonth - 1);
+                  }
+                }}
+                className="px-4 py-2 bg-gray-500 text-white rounded-l-md border-r"
+              >
+                <ChevronLeftCircle />
+              </button>
+              <button
+                onClick={() => {
+                  if (currentMonth === 11) {
+                    setCurrentMonth(0);
+                    setCurrentYear(currentYear + 1);
+                  } else {
+                    setCurrentMonth(currentMonth + 1);
+                  }
+                }}
+                className="px-4 py-2 bg-gray-500 text-white rounded-r-md border-l"
+              >
+                <ChevronRightCircle />
+              </button>
+            </span>
           </div>
         </div>
       </header>
@@ -154,21 +266,25 @@ const Calendar: React.FC = () => {
             <div
               key={index}
               aria-label={`Day ${day.getDate()}`}
-              style={{ background: day.toDateString() === today.toDateString() ? '#fbbf24' : '' }}
+              style={{
+                background:
+                  day.toDateString() === today.toDateString() ? '#fbbf24' : '',
+              }}
               className="flex flex-col justify-start items-end pb-6 pr-2 border bg-white cursor-pointer hover:bg-orange-50 relative"
-              onClick={() => setSelectedDate(day)}
+              onClick={() => {
+                setSelectedDate(day);
+                setModalDisp(true);
+              }}
             >
-              <div className="text-xl font-thin" >{day.getDate()}</div>
+              <div className="text-xl font-thin">{day.getDate()}</div>
               <div className="text-xs text-gray-600">
-                {dayEvents.map((event, idx) => (
-                  (idx < 2) ? (
-                      <div key={idx} className="bg-green-200 p-1 rounded-md mt-1">
-                        {event.description}
-                      </div>
-
-                    )
-                    : null
-                ))}
+                {dayEvents.map((event, idx) =>
+                  idx < 2 ? (
+                    <div key={idx} className="bg-green-200 p-1 rounded-md mt-1">
+                      {event.description}
+                    </div>
+                  ) : null,
+                )}
               </div>
               {selectedDate?.toDateString() === day.toDateString() && (
                 <div className="absolute top-1 left-1 bg-blue-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
